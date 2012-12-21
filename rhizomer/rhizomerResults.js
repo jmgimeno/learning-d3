6620eb093e40d42baec07aa55d87573f510c08ef
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2012-12-21 13:37:59 +0100

    First working version of multi column plot.

---
+++ rhizomer/rhizomerResults.js
@@ -0,0 +1,184 @@
+var oneVariable20Values = {
+  "head": {
+    "vars": [ "r1" , "label" , "value0" ]
+  } ,
+  "results": {
+    "bindings": [
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#AaronRodgers" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Aaron Rodgers" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "295" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#AlexSmith" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Alex Smith" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "236" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#AndyDalton" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Andy Dalton" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "287" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#BenRoethlisberger" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Ben Roethlisberger" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "354" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#BlaineGabbert" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Blaine Gabbert" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "194" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#CamNewton" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Cam Newton" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "327" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#ColtMcCoy" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Colt McCoy" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "335" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#CurtisPainter" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Curtis Painter" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "214" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#DonovanMcNabb" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Donovan McNabb" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "156" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#DrewBrees" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Drew Brees" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "422" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#EliManning" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Eli Manning" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "320" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#JasonCampbell" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Jason Campbell" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "165" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#JayCutler" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Jay Cutler" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "283" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#JoeFlacco" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Joe Flacco" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "361" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#JohnBeck" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "John Beck" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "132" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#JoshFreeman" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Josh Freeman" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "340" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#KevinKolb" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Kevin Kolb" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "227" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#KyleOrton" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Kyle Orton" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "155" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#MarkSanchez" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Mark Sanchez" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "298" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#MattCassel" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Matt Cassel" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "269" }
+      }
+    ]
+  }
+}
+
+var twoVariables10Values = {
+  "head": {
+    "vars": [ "r1" , "label" , "value0" , "value1" ]
+  } ,
+  "results": {
+    "bindings": [
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#AaronRodgers" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Aaron Rodgers" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "295" } ,
+        "value1": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "215" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#AlexSmith" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Alex Smith" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "236" } ,
+        "value1": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "151" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#AndyDalton" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Andy Dalton" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "287" } ,
+        "value1": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "173" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#BenRoethlisberger" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Ben Roethlisberger" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "354" } ,
+        "value1": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "224" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#BlaineGabbert" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Blaine Gabbert" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "194" } ,
+        "value1": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "93" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#CamNewton" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Cam Newton" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "327" } ,
+        "value1": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "197" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#ColtMcCoy" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Colt McCoy" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "335" } ,
+        "value1": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "197" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#CurtisPainter" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Curtis Painter" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "214" } ,
+        "value1": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "117" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#DonovanMcNabb" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Donovan McNabb" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "156" } ,
+        "value1": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "94" }
+      } ,
+      {
+        "r1": { "type": "uri" , "value": "http://www.nfl.com/Stats/passes#DrewBrees" } ,
+        "label": { "datatype": "http://www.w3.org/2001/XMLSchema#string" , "type": "typed-literal" , "value": "Drew Brees" } ,
+        "value0": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "422" } ,
+        "value1": { "datatype": "http://www.w3.org/2001/XMLSchema#integer" , "type": "typed-literal" , "value": "299" }
+      }
+    ]
+  }
+};
+
+var oneTranslator = { "value0" : "Hasattempted" };
+
+var twoTranslator = { "value0" : "Hasattempted", "value1" : "Hascompleted" };
+
